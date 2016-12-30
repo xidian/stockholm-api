b6de0eee50c57090b6cf758ba7226b1e0d7e9b1c
linkmaxzhang <link_max@sina.com>
2016-12-30 12:51:29 +0800

    little chage in NewsService

--- api/src/main/java/com/stockholm/api/setting/news/NewsService.java
+++ api/src/main/java/com/stockholm/api/setting/news/NewsService.java
@@ -1,8 +1,11 @@
 package com.stockholm.api.setting.news;
 
 
+import java.util.Map;
+
+import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
+import retrofit2.http.HeaderMap;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import rx.Observable;
@@ -10,13 +13,11 @@ import rx.Observable;
 public interface NewsService {
 
     @GET("/mobile/clocks/apps/{appid}")
-    Observable<AllChannelResp> getMobileNewsChannel(@Header("Access-Token") String Access_Token,
-                                                    @Header("UUID") String uuid,
+    Observable<AllChannelResp> getMobileNewsChannel(@HeaderMap Map<String, String> headerMap,
                                                     @Path("appid") String appid);
 
     @PUT("/mobile/clocks/apps/{appid}")
-    Observable<SetChannelResp> setMobileNewsChannel(@Header("Access-Token") String Access_Token,
-                                                    @Header("UUID") String uuid,
-                                                    SetChannelReq setChannelReq);
+    Observable<SetChannelResp> setMobileNewsChannel(@HeaderMap Map<String, String> headerMap,
+                                                    @Body SetChannelReq setChannelReq);
 
 }
