6098b4a2cfd75f127cbae030e40ee4241eb2781b
linkmaxzhang <link_max@sina.com>
2016-12-30 11:51:32 +0800

    little chage in NewsService

--- api/src/main/java/com/stockholm/api/setting/news/NewsService.java
+++ api/src/main/java/com/stockholm/api/setting/news/NewsService.java
@@ -9,11 +9,14 @@ import rx.Observable;
 
 public interface NewsService {
 
-    @GET("/mobile/clocks/{clockid}/apps/{appid}")
+    @GET("/mobile/clocks/apps/{appid}")
     Observable<AllChannelResp> getMobileNewsChannel(@Header("Access-Token") String Access_Token,
-                                                    @Path("clockid") String clockid, @Path("appid") String appid);
+                                                    @Header("UUID") String uuid,
+                                                    @Path("appid") String appid);
 
-    @PUT("/mobile/clocks/{clockid}/apps/{appid}")
-    Observable<SetChannelResp> setMobileNewsChannel(@Header("Access-Token") String Access_Token, SetChannelReq setChannelReq);
+    @PUT("/mobile/clocks/apps/{appid}")
+    Observable<SetChannelResp> setMobileNewsChannel(@Header("Access-Token") String Access_Token,
+                                                    @Header("UUID") String uuid,
+                                                    SetChannelReq setChannelReq);
 
 }
