3f4725c0e4ae1780fcbd61c639f467047f5e6c11
linkmaxzhang <link_max@sina.com>
2017-01-20 15:05:05 +0800

    add qinniu token api

---
+++ api/src/main/java/com/stockholm/api/setting/album/AlbumTokenReq.java
@@ -0,0 +1,18 @@
+package com.stockholm.api.setting.album;
+
+public class AlbumTokenReq {
+
+    private int count;
+
+    public AlbumTokenReq(int count){
+        setCount(count);
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
---
+++ api/src/main/java/com/stockholm/api/setting/album/AlbumTokenService.java
@@ -0,0 +1,15 @@
+package com.stockholm.api.setting.album;
+
+import java.util.Map;
+
+import retrofit2.http.Body;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.POST;
+import rx.Observable;
+
+public interface AlbumTokenService {
+
+    @POST("/mobile/apps_configs/album_images")
+    Observable<AlbumTokenResp> getQiniuToken(@HeaderMap Map<String, String> headerMap,
+                                               @Body AlbumTokenReq albumTokenReq);
+}
