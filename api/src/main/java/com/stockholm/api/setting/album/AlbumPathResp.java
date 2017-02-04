520ecc0fe480365e9608b49373e818db1e72d388
linkmaxzhang <link_max@sina.com>
2017-02-04 18:22:24 +0800

    add getImagePath in AlbumService

--- api/src/main/java/com/stockholm/api/setting/album/AlbumPathResp.java
+++ api/src/main/java/com/stockholm/api/setting/album/AlbumPathResp.java
@@ -4,31 +4,62 @@ import java.util.List;
 
 public class AlbumPathResp {
 
-    private boolean success;
-    private String msg;
-    private List<String> data;
+    private ConfigBean config;
+    private List<String> downloadUrls;
 
-    public boolean isSuccess() {
-        return success;
+    public ConfigBean getConfig() {
+        return config;
     }
 
-    public void setSuccess(boolean success) {
-        this.success = success;
+    public void setConfig(ConfigBean config) {
+        this.config = config;
     }
 
-    public String getMsg() {
-        return msg;
+    public List<String> getDownloadUrls() {
+        return downloadUrls;
     }
 
-    public void setMsg(String msg) {
-        this.msg = msg;
+    public void setDownloadUrls(List<String> downloadUrls) {
+        this.downloadUrls = downloadUrls;
     }
 
-    public List<String> getData() {
-        return data;
-    }
+    public static class ConfigBean {
 
-    public void setData(List<String> data) {
-        this.data = data;
+        private boolean showTime;
+        private boolean translation;
+        private boolean autoPlay;
+        private int playInterval;
+
+        public boolean isShowTime() {
+            return showTime;
+        }
+
+        public void setShowTime(boolean showTime) {
+            this.showTime = showTime;
+        }
+
+        public boolean isTranslation() {
+            return translation;
+        }
+
+        public void setTranslation(boolean translation) {
+            this.translation = translation;
+        }
+
+        public boolean isAutoPlay() {
+            return autoPlay;
+        }
+
+        public void setAutoPlay(boolean autoPlay) {
+            this.autoPlay = autoPlay;
+        }
+
+        public int getPlayInterval() {
+            return playInterval;
+        }
+
+        public void setPlayInterval(int playInterval) {
+            this.playInterval = playInterval;
+        }
     }
 }
