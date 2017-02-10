904307f84865adea9d99a66c15ef224d276afafc
linkmaxzhang <link_max@outlook.com>
2017-02-10 15:41:04 +0800

    add unbind service and rename device service

---
+++ api/src/main/java/com/stockholm/api/setting/RenameDeviceReq.java
@@ -0,0 +1,14 @@
+package com.stockholm.api.setting;
+
+public class RenameDeviceReq {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
