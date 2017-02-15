6568ec97011cd96930052377dc646f3f0f721ab3
wangxin <xinyer0510@gmail.com>
2017-02-15 10:54:52 +0800

    update weather service.

--- api/src/main/java/com/stockholm/api/weather/City.java
+++ api/src/main/java/com/stockholm/api/weather/City.java
@@ -1,30 +1,21 @@
 package com.stockholm.api.weather;
 
 
+import java.util.List;
+
 public class City {
 
     public String desc;
     public int status;
-    public Data[] data;
+    public List<Data> data;
 
     public boolean isOk() {
         return desc.equals("OK") && status == 1000;
     }
 
-    public String getCityId() {
-        if (data != null && data.length > 0) {
-            return data[0].cityid;
-        }
 
-        return "";
-    }
-
-    public String getCityName() {
-        if (data != null && data.length > 0) {
-            return data[0].name;
-        }
-
-        return "";
+    public List<Data> getData() {
+        return data;
     }
 
     private static class Data {
