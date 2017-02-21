b214c5d70eefa181f6c6383f37d78436aa811c36
xiao <xych45@gmail.com>
2017-02-21 13:16:46 +0800

    fix api issue

--- api/src/main/java/com/stockholm/api/profile/ProfileService.java
+++ api/src/main/java/com/stockholm/api/profile/ProfileService.java
@@ -6,11 +6,8 @@ import com.stockholm.api.login.CheckVerifiCodeResp;
 import com.stockholm.api.login.SendVerifiCodeReq;
 import com.stockholm.api.login.SendVerifiCodeResp;
 
-import java.util.Map;
-
 import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.HeaderMap;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import rx.Observable;
@@ -18,19 +15,19 @@ import rx.Observable;
 public interface ProfileService {
 
     @PUT("/user/username")
-    Observable<UpdateProfileResp> updateUserName(@HeaderMap Map<String, String> headerMap, @Body UpdateUserNameReq req);
+    Observable<UpdateProfileResp> updateUserName(@Body UpdateUserNameReq req);
 
     @PUT("/user/phone_number")
-    Observable<UpdateProfileResp> updatePhoneNumber(@HeaderMap Map<String, String> headerMap, @Body UpdatePhoneReq req);
+    Observable<UpdateProfileResp> updatePhoneNumber(@Body UpdatePhoneReq req);
 
     @PUT("/user/password")
-    Observable<UpdateProfileResp> updatePwd(@HeaderMap Map<String, String> headerMap, @Body UpdatePwdReq req);
+    Observable<UpdateProfileResp> updatePwd(@Body UpdatePwdReq req);
 
     @POST("/user/avatar")
-    Observable<AvatarTokenResp> getAvatarToken(@HeaderMap Map<String, String> headerMap);
+    Observable<AvatarTokenResp> getAvatarToken();
 
     @GET("/user/avatar")
-    Observable<GetAvatarResp> getAvatar(@HeaderMap Map<String, String> headerMap);
+    Observable<GetAvatarResp> getAvatar();
 
     @POST("verification_codes")
     Observable<SendVerifiCodeResp> sendVerifiCode(@Body SendVerifiCodeReq verifiCodeReq);
