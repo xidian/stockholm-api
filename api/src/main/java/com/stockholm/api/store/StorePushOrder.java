6c80e2c312225e92ebec7923bb79ba5d528b9d3a
xiao <xych45@gmail.com>
2017-03-16 14:17:04 +0800

    remove update code

--- api/src/main/java/com/stockholm/api/store/StorePushOrder.java
+++ api/src/main/java/com/stockholm/api/store/StorePushOrder.java
@@ -5,13 +5,10 @@ public final class StorePushOrder {
 
     public static final int STORE_PUSH_ORDER_START = 100;
 
-    public static final int DOWNLOAD_FAIL = 102;
     public static final int DEVICE_INSTALL_SUCCESS = 103;
     public static final int DEVICE_INSTALL_FAIL = 104;
-    public static final int DEVICE_UPDATE_SUCCESS = 105;
-    public static final int DEVICE_UPDATE_FAIL = 106;
-    public static final int DEVICE_UNINSTALL_SUCCESS = 107;
-    public static final int DEVICE_UNINSTALL_FAIL = 108;
+    public static final int DEVICE_UNINSTALL_SUCCESS = 105;
+    public static final int DEVICE_UNINSTALL_FAIL = 106;
 
     public static final int STORE_PUSH_ORDER_END = 110;
 
