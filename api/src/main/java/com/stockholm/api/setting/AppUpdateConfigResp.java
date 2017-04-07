55df86d23e8dc1c70278a115a554c05cedb0211a
xiao <xych45@gmail.com>
2017-04-07 17:01:24 +0800

    add simple callbacks, update config update api

--- api/src/main/java/com/stockholm/api/setting/AppUpdateConfigResp.java
+++ api/src/main/java/com/stockholm/api/setting/AppUpdateConfigResp.java
@@ -1,10 +1,18 @@
 package com.stockholm.api.setting;
 
+import com.google.gson.Gson;
+
 public class AppUpdateConfigResp {
 
     private boolean success;
     private String msg;
     private String data;
+
+    public AppUpdateConfigResp(String msg, String data) {
+        setSuccess(true);
+        setMsg(msg);
+        setData(data);
+    }
 
     public boolean isSuccess() {
         return success;
@@ -28,5 +36,10 @@ public class AppUpdateConfigResp {
 
     public void setData(String data) {
         this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
     }
 }
--- api/src/main/java/com/stockholm/api/socket/MinaSocketHelper.java
+++ api/src/main/java/com/stockholm/api/socket/MinaSocketHelper.java
@@ -2,7 +2,7 @@ package com.stockholm.api.socket;
 
 
 import org.apache.mina.core.future.ConnectFuture;
-import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.service.IoHandler;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
@@ -17,9 +17,10 @@ import java.nio.charset.Charset;
 public class MinaSocketHelper {
 
     private static MinaSocketHelper helper;
-    private DeviceServiceReceiver serviceReceiver;
-    private MobileServiceReceiver mobileReceiver;
+
     private ConnectFuture connectFuture;
+    private NioSocketConnector clientConnect;
+    private NioSocketAcceptor socketAcceptor;
 
     public static MinaSocketHelper getInstance() {
         if (helper == null) {
@@ -36,43 +37,101 @@ public class MinaSocketHelper {
 
     }
 
-    public void setDeviceMessageReceiver(DeviceServiceReceiver serviceReceiver) {
-        this.serviceReceiver = serviceReceiver;
-    }
-
-    public void setMobileMessageReceiver(MobileServiceReceiver mobileReceiver) {
-        this.mobileReceiver = mobileReceiver;
-    }
-
-    public void startDeviceService(int port) throws IOException {
-        NioSocketAcceptor socketAcceptor = new NioSocketAcceptor();
+    public void startDeviceService(int port, final IoHandlerCallback deviceIoHandler) throws IOException {
+        socketAcceptor = new NioSocketAcceptor();
         socketAcceptor.getSessionConfig().setReadBufferSize(2048);
         socketAcceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
         socketAcceptor.getFilterChain().addLast("codec",
                 new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
-        socketAcceptor.setHandler(new IoHandlerAdapter() {
+        socketAcceptor.setHandler(new IoHandler() {
             @Override
-            public void messageReceived(IoSession session, Object message) throws Exception {
-                serviceReceiver.onMessageReceived(message.toString());
+            public void sessionCreated(IoSession ioSession) throws Exception {
+                deviceIoHandler.sessionCreated(ioSession);
             }
 
             @Override
-            public void messageSent(IoSession session, Object message) throws Exception {
+            public void sessionOpened(IoSession ioSession) throws Exception {
+                deviceIoHandler.sessionOpened(ioSession);
+            }
 
+            @Override
+            public void sessionClosed(IoSession ioSession) throws Exception {
+                deviceIoHandler.sessionClosed(ioSession);
+            }
+
+            @Override
+            public void sessionIdle(IoSession ioSession, IdleStatus idleStatus) throws Exception {
+                deviceIoHandler.sessionIdle(ioSession, idleStatus);
+            }
+
+            @Override
+            public void exceptionCaught(IoSession ioSession, Throwable throwable) throws Exception {
+                deviceIoHandler.exceptionCaught(ioSession, throwable);
+            }
+
+            @Override
+            public void messageReceived(IoSession ioSession, Object o) throws Exception {
+                deviceIoHandler.messageReceived(ioSession, o);
+            }
+
+            @Override
+            public void messageSent(IoSession ioSession, Object o) throws Exception {
+                deviceIoHandler.messageSent(ioSession, o);
+            }
+
+            @Override
+            public void inputClosed(IoSession ioSession) throws Exception {
+                deviceIoHandler.inputClosed(ioSession);
             }
         });
         socketAcceptor.bind(new InetSocketAddress(port));
     }
 
-    public void startMobileService(String address, int port) {
-        NioSocketConnector clientConnect = new NioSocketConnector();
+    public void startMobileService(String address, int port, final IoHandlerCallback mobileIoHandler) {
+        clientConnect = new NioSocketConnector();
         clientConnect.getFilterChain().addLast("codec",
                 new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
         clientConnect.setConnectTimeoutCheckInterval(15);
-        clientConnect.setHandler(new IoHandlerAdapter() {
+        clientConnect.setHandler(new IoHandler() {
             @Override
-            public void messageReceived(IoSession session, Object message) throws Exception {
-                mobileReceiver.onMessageReceived(message.toString());
+            public void sessionCreated(IoSession ioSession) throws Exception {
+                mobileIoHandler.sessionCreated(ioSession);
+            }
+
+            @Override
+            public void sessionOpened(IoSession ioSession) throws Exception {
+                mobileIoHandler.sessionOpened(ioSession);
+            }
+
+            @Override
+            public void sessionClosed(IoSession ioSession) throws Exception {
+                ioSession.closeNow();
+                mobileIoHandler.sessionClosed(ioSession);
+            }
+
+            @Override
+            public void sessionIdle(IoSession ioSession, IdleStatus idleStatus) throws Exception {
+                mobileIoHandler.sessionIdle(ioSession, idleStatus);
+            }
+
+            @Override
+            public void exceptionCaught(IoSession ioSession, Throwable throwable) throws Exception {
+                mobileIoHandler.exceptionCaught(ioSession, throwable);
+            }
+
+            @Override
+            public void messageReceived(IoSession ioSession, Object o) throws Exception {
+                mobileIoHandler.messageReceived(ioSession, o);
+            }
+
+            @Override
+            public void messageSent(IoSession ioSession, Object o) throws Exception {
+                mobileIoHandler.messageSent(ioSession, o);
+            }
+
+            @Override
+            public void inputClosed(IoSession ioSession) throws Exception {
+                mobileIoHandler.inputClosed(ioSession);
             }
         });
 
@@ -80,25 +139,21 @@ public class MinaSocketHelper {
         connectFuture = clientConnect.connect(new InetSocketAddress(address, port));
         // 等待连接创建完成
         connectFuture.awaitUninterruptibly();
-
-        // 等待连接断开
-        connectFuture.getSession().getCloseFuture().awaitUninterruptibly();
-        // 释放连接
-        clientConnect.dispose();
     }
 
-    public void sendMsgToService(String message) {
-        if (connectFuture.isConnected()) {
-            connectFuture.getSession().write(message);
+    public void closeDeviceSocket() {
+        if (socketAcceptor != null && socketAcceptor.isActive()) {
+            socketAcceptor.unbind();
         }
     }
 
-    public interface DeviceServiceReceiver {
-        void onMessageReceived(String message);
-    }
-
-    public interface MobileServiceReceiver {
-        void onMessageReceived(String message);
+    public void closeMobileSocket() {
+        if (connectFuture != null && connectFuture.isConnected()) {
+            // 等待连接断开
+            connectFuture.getSession().getCloseFuture().awaitUninterruptibly();
+            // 释放连接
+            clientConnect.dispose();
+        }
     }
 
 }
