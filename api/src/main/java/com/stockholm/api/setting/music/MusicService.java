567dc93c876bc01492774ca1675f6dd15d7221a2
linkmax <link_max@sina.com>
2017-04-28 16:16:40 +0800

    remove header map in SubscribeResp

--- api/src/main/java/com/stockholm/api/setting/music/MusicService.java
+++ api/src/main/java/com/stockholm/api/setting/music/MusicService.java
@@ -1,15 +1,11 @@
 package com.stockholm.api.setting.music;
 
-import java.util.Map;
-
 import retrofit2.http.Body;
-import retrofit2.http.HeaderMap;
 import retrofit2.http.POST;
 import rx.Observable;
 
 public interface MusicService {
 
     @POST("/mobile/clock/app/check_subscription")
-    Observable<SubscribeResp> checkSubscribe(@HeaderMap Map<String, String> headerMap,
-                                             @Body SubscribeReq subscribeReq);
+    Observable<SubscribeResp> checkSubscribe(@Body SubscribeReq subscribeReq);
 }
