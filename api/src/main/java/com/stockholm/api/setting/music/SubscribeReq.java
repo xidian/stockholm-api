5a473802e7c804eaf2577071646cc6031609bd42
linkmax <link_max@sina.com>
2017-04-28 10:39:15 +0800

    add music service for subscribe checking

--- api/src/main/java/com/stockholm/api/setting/music/SubscribeReq.java
+++ api/src/main/java/com/stockholm/api/setting/music/SubscribeReq.java
@@ -2,4 +2,22 @@ package com.stockholm.api.setting.music;
 
 public class SubscribeReq {
 
+    private String key;
+    private String item;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
 }
--- api/src/main/java/com/stockholm/api/setting/music/SubscribeResp.java
+++ api/src/main/java/com/stockholm/api/setting/music/SubscribeResp.java
@@ -2,4 +2,22 @@ package com.stockholm.api.setting.music;
 
 public class SubscribeResp {
 
+    private boolean success;
+    private String msg;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
