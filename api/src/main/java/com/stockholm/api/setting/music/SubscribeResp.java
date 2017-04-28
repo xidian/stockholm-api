67254c246501584c43d1e14c6ff3137911631b0c
linkmax <link_max@sina.com>
2017-04-28 15:46:05 +0800

    modify the SubscribeResp

--- api/src/main/java/com/stockholm/api/setting/music/SubscribeResp.java
+++ api/src/main/java/com/stockholm/api/setting/music/SubscribeResp.java
@@ -4,6 +4,7 @@ public class SubscribeResp {
 
     private boolean success;
     private String msg;
+    private DataBean data;
 
     public boolean isSuccess() {
         return success;
@@ -19,5 +20,26 @@ public class SubscribeResp {
 
     public void setMsg(String msg) {
         this.msg = msg;
+    }
+
+    public DataBean getData() {
+        return data;
+    }
+
+    public void setData(DataBean data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+
+        private boolean subscribed;
+
+        public boolean isSubscribed() {
+            return subscribed;
+        }
+
+        public void setSubscribed(boolean subscribed) {
+            this.subscribed = subscribed;
+        }
     }
 }
