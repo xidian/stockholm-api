511431f0b73d1bc7f4a17250bcd52025662371ba
linkmax <link_max@sina.com>
2017-06-06 16:43:21 +0800

    implements parcelable in MeowTaskBean

--- api/src/main/java/com/stockholm/api/task/MeowTaskBean.java
+++ api/src/main/java/com/stockholm/api/task/MeowTaskBean.java
@@ -1,7 +1,10 @@
 package com.stockholm.api.task;
 
 
-public class MeowTaskBean {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MeowTaskBean implements Parcelable{
 
     private String packageName;
     private String taskId;
@@ -30,4 +33,38 @@ public class MeowTaskBean {
     public void setPriority(int priority) {
         this.priority = priority;
     }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.packageName);
+        dest.writeString(this.taskId);
+        dest.writeInt(this.priority);
+    }
+
+    public MeowTaskBean() {
+    }
+
+    protected MeowTaskBean(Parcel in) {
+        this.packageName = in.readString();
+        this.taskId = in.readString();
+        this.priority = in.readInt();
+    }
+
+    public static final Creator<MeowTaskBean> CREATOR = new Creator<MeowTaskBean>() {
+        @Override
+        public MeowTaskBean createFromParcel(Parcel source) {
+            return new MeowTaskBean(source);
+        }
+
+        @Override
+        public MeowTaskBean[] newArray(int size) {
+            return new MeowTaskBean[size];
+        }
+    };
 }
