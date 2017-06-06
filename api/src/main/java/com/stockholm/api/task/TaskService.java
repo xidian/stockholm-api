80686237432ebb9cf66e42cafc56fd9a81747beb
linkmax <link_max@sina.com>
2017-06-06 17:34:57 +0800

    remove access-token in TaskService

--- api/src/main/java/com/stockholm/api/task/TaskService.java
+++ api/src/main/java/com/stockholm/api/task/TaskService.java
@@ -10,8 +10,7 @@ import rx.Observable;
 public interface TaskService {
 
     @PUT("/mobile/clock/app_with_task")
-    Observable<MeowTaskResp> modify(@Header("Access-Token") String accessToken,
-                                    @Header("UUID") String uuid,
+    Observable<MeowTaskResp> modify(@Header("UUID") String uuid,
                                     @Header("PackageName") String packageName,
                                     @Body MeowTaskReq req);
 
