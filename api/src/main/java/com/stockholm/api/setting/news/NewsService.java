850a527dffba784255d567116152c84629eb3c4e
xiao <xych45@gmail.com>
2017-06-20 18:36:08 +0800

    [news] update news api

--- api/src/main/java/com/stockholm/api/setting/news/NewsService.java
+++ api/src/main/java/com/stockholm/api/setting/news/NewsService.java
@@ -5,7 +5,6 @@ import retrofit2.http.GET;
 import retrofit2.http.HTTP;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 import rx.Observable;
 
 public interface NewsService {
@@ -16,16 +15,16 @@ public interface NewsService {
     @GET("app/news/subscriptions")
     Observable<NewsConfigResp> getNewsConfig();
 
-    @HTTP(method = "DELETE" , path = "/app/news/channels/{categoryId}/unsubscribe")
+    @POST("/app/news/channels/{categoryId}/subscribe")
     Observable<NewsCommonResp> subscribeChannel(@Path("categoryId") long categoryId);
 
-    @POST("/app/news/channels/{categoryId}/subscribe")
+    @HTTP(method = "DELETE" , path = "/app/news/channels/{categoryId}/unsubscribe")
     Observable<NewsCommonResp> unSubscribeChannel(@Path("categoryId") long categoryId);
 
-    @GET("")
+    @GET("app/news/contents")
     Observable<DeviceNewsListResp> deviceAllNews();
 
-    @GET("")
-    Observable<DeviceNewsListResp> getNewsByCategotyId(@Query("categoryId") long categoryId);
+    @GET("/app/news/channels/{categoryId}/contents")
+    Observable<DeviceNewsListResp> getNewsByCategotyId(@Path("categoryId") long categoryId);
 
 }
