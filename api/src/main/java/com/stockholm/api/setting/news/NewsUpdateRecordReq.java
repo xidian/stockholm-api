dce88e98c2169c0166736cc84a8012536dc62a0f
xiao <xych45@gmail.com>
2017-06-24 10:15:06 +0800

    [news] update news record request bean

--- api/src/main/java/com/stockholm/api/setting/news/NewsUpdateRecordReq.java
+++ api/src/main/java/com/stockholm/api/setting/news/NewsUpdateRecordReq.java
@@ -1,21 +1,50 @@
 package com.stockholm.api.setting.news;
 
 
-import com.stockholm.api.audio.AudioRecord;
-
 public class NewsUpdateRecordReq {
-    private AudioRecord record;
 
-    public NewsUpdateRecordReq(AudioRecord record) {
+    private NewsRecord record;
+
+    public NewsUpdateRecordReq(NewsRecord record) {
         this.record = record;
     }
 
-    public AudioRecord getRecord() {
+    public NewsRecord getRecord() {
         return record;
     }
 
-    public void setRecord(AudioRecord record) {
+    public void setRecord(NewsRecord record) {
         this.record = record;
+    }
+
+    public static class NewsRecord {
+        private long newsId;
+        private int playState;
+        private String extra;
+
+        public long getNewsId() {
+            return newsId;
+        }
+
+        public void setNewsId(long newsId) {
+            this.newsId = newsId;
+        }
+
+        public int getPlayState() {
+            return playState;
+        }
+
+        public void setPlayState(int playState) {
+            this.playState = playState;
+        }
+
+        public String getExtra() {
+            return extra;
+        }
+
+        public void setExtra(String extra) {
+            this.extra = extra;
+        }
     }
 
 }
