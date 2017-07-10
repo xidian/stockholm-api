ec0f671c1787271c81986a180aeec232354dfc76
xiao <xych45@gmail.com>
2017-07-10 21:27:56 +0800

    [clock] update clock config api

--- api/src/main/java/com/stockholm/api/setting/clock/ClockConfigUpdateReq.java
+++ api/src/main/java/com/stockholm/api/setting/clock/ClockConfigUpdateReq.java
@@ -2,18 +2,60 @@ package com.stockholm.api.setting.clock;
 
 
 public class ClockConfigUpdateReq {
-    private ClockConfigBean clock;
+    private ClockUpdateBean clock;
 
-    public ClockConfigUpdateReq(ClockConfigBean clock) {
-        this.clock = clock;
+    public ClockConfigUpdateReq() {
     }
 
-    public ClockConfigBean getClock() {
+    public ClockConfigUpdateReq(boolean skinAutoChange, boolean openSecondSound, long skin) {
+        setClock(new ClockUpdateBean(skinAutoChange, openSecondSound, skin));
+    }
+
+    public ClockUpdateBean getClock() {
         return clock;
     }
 
-    public void setClock(ClockConfigBean clock) {
+    public void setClock(ClockUpdateBean clock) {
         this.clock = clock;
+    }
+
+    public static class ClockUpdateBean {
+        private boolean skinAutoChange;
+        private boolean openSecondSound;
+        private long skin;
+
+        public ClockUpdateBean() {
+        }
+
+        public ClockUpdateBean(boolean skinAutoChange, boolean openSecondSound, long skin) {
+            this.skinAutoChange = skinAutoChange;
+            this.openSecondSound = openSecondSound;
+            this.skin = skin;
+        }
+
+        public boolean isSkinAutoChange() {
+            return skinAutoChange;
+        }
+
+        public void setSkinAutoChange(boolean skinAutoChange) {
+            this.skinAutoChange = skinAutoChange;
+        }
+
+        public boolean isOpenSecondSound() {
+            return openSecondSound;
+        }
+
+        public void setOpenSecondSound(boolean openSecondSound) {
+            this.openSecondSound = openSecondSound;
+        }
+
+        public long getSkin() {
+            return skin;
+        }
+
+        public void setSkin(long skin) {
+            this.skin = skin;
+        }
     }
 
 }
