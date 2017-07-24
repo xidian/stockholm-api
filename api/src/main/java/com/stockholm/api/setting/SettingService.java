c9d28b385c678f5ccf7bf8390520c1f3ac37fe35
xiao <xych45@gmail.com>
2017-07-24 15:06:59 +0800

    remove unused code

--- api/src/main/java/com/stockholm/api/setting/SettingService.java
+++ api/src/main/java/com/stockholm/api/setting/SettingService.java
@@ -4,13 +4,9 @@ package com.stockholm.api.setting;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
-import retrofit2.http.PUT;
 import rx.Observable;
 
 public interface SettingService {
-
-    @PUT("/device/apps_config")
-    Observable<DeviceUpdateConfigResp> deviceUpdateConfig(@Body DeviceUpdateConfigReq config);
 
     @GET("/device/apps_config")
     Observable<DeviceGetConfigResp> deviceGetConfig();
