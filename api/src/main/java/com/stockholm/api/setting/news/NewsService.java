049ead41027c82e91b7122fd5f0341820d1ab07e
linkmaxzhang <linkjiancheung@gmail.com>
2017-08-01 15:18:36 +0800

    [news]add error code in new record

--- api/src/main/java/com/stockholm/api/setting/news/NewsService.java
+++ api/src/main/java/com/stockholm/api/setting/news/NewsService.java
@@ -1,6 +1,10 @@
 package com.stockholm.api.setting.news;
 
 
+import com.stockholm.api.audio.AudioRecord;
+import com.stockholm.api.base.BaseResponse;
+
+import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.HTTP;
@@ -20,7 +24,7 @@ public interface NewsService {
     @POST("/app/news/channels/{categoryId}/subscribe")
     Observable<NewsCommonResp> subscribeChannel(@Path("categoryId") long categoryId);
 
-    @HTTP(method = "DELETE" , path = "/app/news/channels/{categoryId}/unsubscribe")
+    @HTTP(method = "DELETE", path = "/app/news/channels/{categoryId}/unsubscribe")
     Observable<NewsCommonResp> unSubscribeChannel(@Path("categoryId") long categoryId);
 
     @GET("/app/news/contents")
@@ -30,7 +34,7 @@ public interface NewsService {
     Observable<DeviceNewsListResp> getNewsByCategotyId(@Path("categoryId") long categoryId);
 
     @GET("/app/news/record")
-    Observable<GetNewsRecordResp> getNewsAudioRecord();
+    Observable<Response<BaseResponse<AudioRecord>>> getNewsAudioRecord();
 
     @PUT("/app/news/record")
     Observable<NewsCommonResp> updateNewsAudioRecord(@Body NewsUpdateRecordReq req);
