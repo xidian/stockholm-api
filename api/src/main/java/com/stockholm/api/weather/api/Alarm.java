bdd9cb950eae7026d461fa2632a73ed9df8ef02e
wangxin <xinyer0510@gmail.com>
2017-08-29 14:35:55 +0800

    [weather] update weather alarm format again T_T.

--- api/src/main/java/com/stockholm/api/weather/api/Alarm.java
+++ api/src/main/java/com/stockholm/api/weather/api/Alarm.java
@@ -3,118 +3,117 @@ package com.stockholm.api.weather.api;
 
 public class Alarm {
 
-
     /**
-     * w7 : 黄色
-     * w8 : 2017-08-26 17:12
-     * w9 : 江门市气象台于08月26日17时12分发布雷雨大风黄色预警信号，请注意防御。
-     * w11 : http://mobile.weathercn.com/alert.do?id=44070041600000_20170826171041
-     * w10 : 44070041600000_20170826171041
-     * w1 : 广东省
-     * w2 : 江门市
-     * w3 :
-     * w4 : gd08
-     * w5 : 雷雨大风
-     * w6 : 03
+     * standard :
+     * icon_big : http://static.etouch.cn/weather/alarm_icon/big/rainstorm_2@3x.png
+     * suggestion :
+     * pub_time : 2017-08-29 13:35:00
+     * degree : 黄色
+     * icon : http://static.etouch.cn/weather/alarm_icon/small/rainstorm_2@3x.png
+     * city_range : 101020100
+     * location : 上海市
+     * details : 上海市气象台发布暴雨黄色预警
+     * type : 暴雨
+     * desc : 上海中心气象台2017年08月29日13时35分发布暴雨黄色预警[Ⅲ级/较重]：受较强的降水云团影响，预计未来6小时内崇明、嘉定、青浦、松江、金山等地有短时强降水，小时雨量30-50毫米，请加强防范。
      */
 
-    private String w7;
-    private String w8;
-    private String w9;
-    private String w11;
-    private String w10;
-    private String w1;
-    private String w2;
-    private String w3;
-    private String w4;
-    private String w5;
-    private String w6;
+    private String standard;
+    private String icon_big;
+    private String suggestion;
+    private String pub_time;
+    private String degree;
+    private String icon;
+    private String city_range;
+    private String location;
+    private String details;
+    private String type;
+    private String desc;
 
-    public String getW7() {
-        return w7;
+    public String getStandard() {
+        return standard;
     }
 
-    public void setW7(String w7) {
-        this.w7 = w7;
+    public void setStandard(String standard) {
+        this.standard = standard;
     }
 
-    public String getW8() {
-        return w8;
+    public String getIcon_big() {
+        return icon_big;
     }
 
-    public void setW8(String w8) {
-        this.w8 = w8;
+    public void setIcon_big(String icon_big) {
+        this.icon_big = icon_big;
     }
 
-    public String getW9() {
-        return w9;
+    public String getSuggestion() {
+        return suggestion;
     }
 
-    public void setW9(String w9) {
-        this.w9 = w9;
+    public void setSuggestion(String suggestion) {
+        this.suggestion = suggestion;
     }
 
-    public String getW11() {
-        return w11;
+    public String getPub_time() {
+        return pub_time;
     }
 
-    public void setW11(String w11) {
-        this.w11 = w11;
+    public void setPub_time(String pub_time) {
+        this.pub_time = pub_time;
     }
 
-    public String getW10() {
-        return w10;
+    public String getDegree() {
+        return degree;
     }
 
-    public void setW10(String w10) {
-        this.w10 = w10;
+    public void setDegree(String degree) {
+        this.degree = degree;
     }
 
-    public String getW1() {
-        return w1;
+    public String getIcon() {
+        return icon;
     }
 
-    public void setW1(String w1) {
-        this.w1 = w1;
+    public void setIcon(String icon) {
+        this.icon = icon;
     }
 
-    public String getW2() {
-        return w2;
+    public String getCity_range() {
+        return city_range;
     }
 
-    public void setW2(String w2) {
-        this.w2 = w2;
+    public void setCity_range(String city_range) {
+        this.city_range = city_range;
     }
 
-    public String getW3() {
-        return w3;
+    public String getLocation() {
+        return location;
     }
 
-    public void setW3(String w3) {
-        this.w3 = w3;
+    public void setLocation(String location) {
+        this.location = location;
     }
 
-    public String getW4() {
-        return w4;
+    public String getDetails() {
+        return details;
     }
 
-    public void setW4(String w4) {
-        this.w4 = w4;
+    public void setDetails(String details) {
+        this.details = details;
     }
 
-    public String getW5() {
-        return w5;
+    public String getType() {
+        return type;
     }
 
-    public void setW5(String w5) {
-        this.w5 = w5;
+    public void setType(String type) {
+        this.type = type;
     }
 
-    public String getW6() {
-        return w6;
+    public String getDesc() {
+        return desc;
     }
 
-    public void setW6(String w6) {
-        this.w6 = w6;
+    public void setDesc(String desc) {
+        this.desc = desc;
     }
 }
