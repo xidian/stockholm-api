32865c6d1cbac6bd9ba812f61e0360e569c335c3
linkmaxzhang <linkjiancheung@gmail.com>
2017-09-01 10:29:13 +0800

    [rom]add rom service

--- api/src/main/java/com/stockholm/api/rom/RomService.java
+++ api/src/main/java/com/stockholm/api/rom/RomService.java
@@ -4,11 +4,11 @@ import com.stockholm.api.base.BaseResponse;
 
 import retrofit2.Response;
 import retrofit2.http.Body;
-import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import rx.Observable;
 
 public interface RomService {
 
-    @POST("/v1/device/rom")
+    @PUT("/v1/device/rom")
     Observable<Response<BaseResponse>> updateVersion(@Body VersionReq req);
 }
