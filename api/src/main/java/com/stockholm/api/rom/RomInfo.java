1d2791be3e6761e0b6b2c75a63630e4ec07bd76f
linkmaxzhang <linkjiancheung@gmail.com>
2017-09-20 13:12:22 +0800

    [rom]modify rom info

--- api/src/main/java/com/stockholm/api/rom/RomInfo.java
+++ api/src/main/java/com/stockholm/api/rom/RomInfo.java
@@ -2,72 +2,17 @@ package com.stockholm.api.rom;
 
 public class RomInfo {
 
-    public RomInfo(String mid, String oem, String models, String platform, String deviceType, String version, String versionName, String launcherVersion) {
-        this.mid = mid;
-        this.oem = oem;
-        this.models = models;
-        this.platform = platform;
-        this.deviceType = deviceType;
+    public RomInfo(String versionName, String version, String launcherVersion) {
         this.versionName = versionName;
         this.version = version;
         this.launcherVersion = launcherVersion;
     }
-
-    private String mid;
-
-    private String oem;
-
-    private String models;
-
-    private String platform;
-
-    private String deviceType;
 
     private String versionName;
 
     private String version;
 
     private String launcherVersion;
-
-    public String getMid() {
-        return mid;
-    }
-
-    public void setMid(String mid) {
-        this.mid = mid;
-    }
-
-    public String getOem() {
-        return oem;
-    }
-
-    public void setOem(String oem) {
-        this.oem = oem;
-    }
-
-    public String getModels() {
-        return models;
-    }
-
-    public void setModels(String models) {
-        this.models = models;
-    }
-
-    public String getPlatform() {
-        return platform;
-    }
-
-    public void setPlatform(String platform) {
-        this.platform = platform;
-    }
-
-    public String getDeviceType() {
-        return deviceType;
-    }
-
-    public void setDeviceType(String deviceType) {
-        this.deviceType = deviceType;
-    }
 
     public String getVersionName() {
         return versionName;
