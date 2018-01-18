1f2d77b42a32d165dc5a6dbec170021649f7b6d1
link_max <link_max@outlook.com>
2018-01-18 15:27:18 +0800

    [auto-task] add auto task api service

---
+++ api/src/main/java/com/stockholm/api/setting/task/TaskBean.java
@@ -0,0 +1,29 @@
+package com.stockholm.api.setting.task;
+
+public class TaskBean {
+
+
+    /**
+     * taskId : f66qEE
+     * packageName : com.stockholm.news
+     */
+
+    private String taskId;
+    private String packageName;
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+}
